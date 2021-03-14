81b4badc06ca2a15ab5290ec5f3abe6b7f2b2ad5
Isaac <ick.seshoka@gmail.com>
2021-03-14 16:47:07 +0200

    add theme provider

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import { theme } from "@styles/theme";
 
 import Router from "@components/router";
 
-const App = () => <Router />;
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+  );
+};
 
 export default App;
